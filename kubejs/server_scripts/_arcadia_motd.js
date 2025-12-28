00f31ac77e2dce2641aac0b914672ccde82c2336
Odey555 <ryan_pagano@hotmail.com>
2025-12-28 06:45:11 -0300

    KubeJS changes & config additions

--- kubejs/server_scripts/_arcadia_motd.js
+++ kubejs/server_scripts/_arcadia_motd.js
@@ -1,4 +1,4 @@
 PlayerEvents.loggedIn(event => {
-    event.player.tell(["Welcome to ", Text.purple("Arcadia Unlimited"), "!"]);
-    event.player.tell(["Please report any issues to ", Text.gold("the Github").underlined().clickOpenUrl("https://github.com/Odey555/ArcadiaUnlimited/issues").hover("Click to open"), "."]);
+    event.player.tell(["Welcome to ", "§d§oArcadia Unlimited", "!"]);
+    event.player.tell(["Please report any issues to ", Text.gold("§6§nthe Github").underlined().clickOpenUrl("https://github.com/Odey555/ArcadiaUnlimited/issues").hover("Click to open"), "."]);
 })
